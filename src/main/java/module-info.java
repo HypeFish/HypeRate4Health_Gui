bc3d2f38318fb16e1eda6b5435b6537c9de36b2f
HypeFish <99514952+HypeFish@users.noreply.github.com>
2023-03-30 21:35:38 -0400

    new commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module hyperate.hyperate4health {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens hyperate.hyperate4health to javafx.fxml;
+    exports hyperate.hyperate4health;
+}
